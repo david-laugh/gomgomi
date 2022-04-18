728ad31ecfc502de3dfdd1eca2749120dbacb311
david-laugh <matda8080@gmail.com>
2022-04-18 22:38:29 +0900

    Start Development Gomgomi App

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const colors = {
+    blue: '#CBDEFF',
+    green: '#C8FFF2',
+    red: '#FFC8E2',
+    yellow: '#FFF0C8',
+    while: '#FFFFFF',
+    black: '#000000',
+    gray: '#949BA5',
+};
+
+export const theme = {
+    // 임시 배경화면
+    background: colors.green,
+    text: colors.black,
+    grayText: colors.gray,
+};
