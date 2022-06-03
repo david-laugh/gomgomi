9cb255d94a84036c074ad5cc774a730e4235aad6
Lee Gyu Hyuk <matda8080@gmail.com>
2022-06-03 10:37:09 +0900

    Update Tab Icon

--- src/navigations/MainTab.js
+++ src/navigations/MainTab.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home, ChatCall, ChatTalk, Profile } from '../screens';
+import { Image } from 'react-native';
+
+const call = require('../../assets/tab/call.png');
+const chat = require('../../assets/tab/chat.png');
+const home = require('../../assets/tab/home.png');
+const profile = require('../../assets/tab/profile.png');
 
 const Tab = createBottomTabNavigator();
 
@@ -12,22 +18,48 @@ const MainTab = () => {
                 component={Home}
                 options={{
                     headerShown: false,
+                    tabBarIcon: () => (
+                        <Image
+                            source={home}
+                        />
+                    )
                 }}
             />
             <Tab.Screen
                 name="ChatCall"
                 component={ChatCall}
-                options={{ headerShown: false }}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: () => (
+                        <Image
+                            source={call}
+                        />
+                    ) 
+                }}
             />
             <Tab.Screen
                 name="ChatTalk"
                 component={ChatTalk}
-                options={{ headerShown: false }}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: () => (
+                        <Image
+                            source={chat}
+                        />
+                    )
+                }}
             />
             <Tab.Screen 
                 name="Profile"
                 component={Profile}
-                options={{headerShown: false}}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: () => (
+                        <Image
+                            source={profile}
+                        />
+                    )
+                }}
             />
         </Tab.Navigator>
     );
