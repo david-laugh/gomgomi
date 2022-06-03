df4ebfef1026a7d1e0795078651b30d201acbd76
Lee Gyu Hyuk <matda8080@gmail.com>
2022-06-03 13:15:01 +0900

    Update Profile 60%

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Image from './Image';
 import Input from './Input';
 import Button from './Button';
 import Spinner from './Spinner';
+import Accordian from './Accordian';
 
-export { Image, Input, Button, Spinner };
+export { Image, Input, Button, Spinner, Accordian };
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components/native';
 import { Text, View, StyleSheet, Dimensions } from 'react-native';
-import { Button } from '../components';
+import { Button, Accordian } from '../components';
 import { theme } from '../theme';
 
 const BG = require('../../assets/BG.png');
+const profile = require('../../assets/profile.png');
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -14,30 +15,117 @@ const Profile = ({ navigation }) => {
         navigation.navigate('EditProfile');
     };
 
+    const menu = [
+        { 
+            title: 'Non Veg Biryanis', 
+            data: 'Biryani also known as biriyani, biriani, birani or briyani, is a mixed rice dish with its origins among the Muslims of the Indian subcontinent. This dish is especially popular throughout the Indian subcontinent, as well as among the diaspora from the region. It is also prepared in other regions such as Iraqi Kurdistan.',
+        },
+        { 
+            title: 'Pizzas',
+            data: 'Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and various other ingredients (anchovies, olives, meat, etc.) baked at a high temperature, traditionally in a wood-fired oven. In formal settings, like a restaurant, pizza is eaten with knife and fork, but in casual settings it is cut into wedges to be eaten while held in the hand. Small pizzas are sometimes called pizzettas.'
+        },
+        { 
+            title: 'Drinks',
+            data: 'A drink (or beverage) is a liquid intended for human consumption. In addition to their basic function of satisfying thirst, drinks play important roles in human culture. Common types of drinks include plain drinking water, milk, coffee, tea, hot chocolate, juice and soft drinks. In addition, alcoholic drinks such as wine, beer, and liquor, which contain the drug ethanol, have been part of human culture for more than 8,000 years.'
+        }
+    ]
+      
+    const renderAccordians=()=> {
+        const items = [];
+        for (let item of menu) {
+            items.push(
+                <Accordian 
+                    title = {item.title}
+                    data = {item.data}
+                />
+            );
+        }
+        return items;
+    }
+
     return (
         <View style={[styles.header]}>
-            <ImageBackground source={BG} imageStyle={styles.bgImageStyle}>
+            <ImageBackground
+                source={BG}
+                imageStyle={styles.bgImageStyle}
+            >
                 <View style={styles.container}>
                     <View style={styles.case1}>
-                        <Text style={{ textAlign: 'center' }}>Profile</Text>
+                        <Text style={styles.textCase1}>Profile</Text>
                     </View>
-                    <View style={styles.case2}></View>
+                    <View style={styles.case2}>
+                        <ImgProfile 
+                            style={{
+                                height: windowWidth * 0.25,
+                                width: windowWidth * 0.25
+                            }}
+                            source={profile}
+                        />
+                    </View>
                     <View style={styles.case3}>
-                        <Text>John Doe</Text>
+                        <Text style={styles.textCase2}>John Doe</Text>
                     </View>
                     <View style={styles.case4}>
                         <Text>JohnDoe@Gomgomi.com</Text>
                     </View>
                     <View style={styles.case5}>
-                        <Button
-                            title="Edit Profile"
-                            onPress={_handleEditProfilePress}
-                        />
+                        <View
+                            style={{
+                                width: '50%',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Button 
+                                containerStyle={{
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    height: '70%',
+                                    width: '80%',
+                                    backgroundColor: '#FFFFFF',
+                                    borderRadius: 15
+                                }}
+                                onPress={_handleChatCallButtonPress}
+                                title="Edit Profile"
+                                titleStyle={{
+                                    fontSize: 17,
+                                    fontWeight: "bold",
+                                    color: '#000000'
+                                }}
+                            />
+                        </View>
+                        <View
+                            style={{
+                                width: '50%',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <Button 
+                                containerStyle={{
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    height: '70%',
+                                    width: '80%',
+                                    backgroundColor: '#FFFFFF',
+                                    borderRadius: 15
+                                }}
+                                onPress={_handleChatCallButtonPress}
+                                title="Log-out"
+                                titleStyle={{
+                                    fontSize: 17,
+                                    fontWeight: "bold",
+                                    color: '#000000'
+                                }}
+                            />
+                        </View>
                     </View>
                     <View style={styles.case6}>
-                        <Text>FAQ</Text>
+                        <Text style={styles.textCase3}>FAQ</Text>
                     </View>
-                    <View style={styles.case7}></View>
+                    <View style={styles.case7}>
+                        <View style={styles.container}>
+                            {/* { renderAccordians() } */}
+                        </View>
+                    </View>
                 </View>
             </ImageBackground>
         </View>
@@ -49,15 +137,17 @@ export default Profile;
 const ImageBackground = styled.ImageBackground`
     flex: 1;
 `;
+const ImgProfile = styled.Image`
+`;
 
 const styles = StyleSheet.create({
     header: { flex: 1 },
     bgImageStyle: {
         bottom: 100,
         width: windowWidth * 1.2,
-        height: windowHeight * 1.2,
+        height: windowHeight * 1,
         left: -windowWidth * 0.1,
-        top: -windowHeight * 0.35,
+        top: -windowHeight * 0.2,
     },
     startChatStyle: {
         left: '8%',
@@ -68,8 +158,21 @@ const styles = StyleSheet.create({
         backgroundColor: '#D6E7FF'
     },
 
+    textCase1: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    textCase2: {
+        fontSize: 23,
+        fontWeight: "bold",
+    },
+    textCase3: {
+        fontSize: 18,
+    },
+
     container: {
         flex: 1,
+        alignItems: 'center',
     },
     case1: {
         width: '100%',
@@ -77,45 +180,46 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'flex-end',
         justifyContent: 'center',
-        backgroundColor: theme.testcase1,
+        // backgroundColor: theme.testcase1,
     },
     case2: {
         width: '100%',
         height: '18%',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: theme.testcase2,
+        // backgroundColor: theme.testcase2,
     },
     case3: {
         width: '100%',
         height: '7%',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: theme.testcase3,
+        // backgroundColor: theme.testcase3,
     },
     case4: {
         width: '100%',
         height: '5%',
         alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: theme.testcase4,
+        // backgroundColor: theme.testcase4,
     },
     case5: {
-        width: '100%',
+        flexDirection: 'row',
+        width: '90%',
         height: '10%',
-        justifyContent: 'center',
-        backgroundColor: theme.testcase5,
+        // backgroundColor: theme.testcase5,
     },
     case6: {
         width: '100%',
         height: '10%',
         paddingLeft: '9%',
         justifyContent: 'center',
-        backgroundColor: theme.testcase6,
+        // backgroundColor: theme.testcase6,
     },
     case7: {
         width: '100%',
         height: '30%',
-        backgroundColor: theme.testcase1,
+        // backgroundColor: theme.testcase1,
     },
 });
+
+const _handleChatCallButtonPress = async () => {};
