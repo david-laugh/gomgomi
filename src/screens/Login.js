c296dcf9f7a98a635c5a7c2908af442fcdf58c19
Lee Gyu Hyuk <matda8080@gmail.com>
2022-06-11 01:55:37 +0900

    Update ApiTest

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,210 +1,196 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useState, useRef, useEffect, useContext } from 'react';
-import { ProgressContext, UserContext } from '../contexts';
-import styled from 'styled-components/native';
-import { Input, Button } from '../components';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { validateEmail, removeWhitespace } from '../utils/common';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { View, StyleSheet, Text } from 'react-native';
-import { theme } from '../theme';
-
-const Gomgomi = require('../../assets/gomgomi_head.png');
+import { ScrollView, Button, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
+import base64 from 'react-native-base64';
+// import { readFile } from "react-native-fs";
 
 const Login = ({ navigation }) => {
-    const { login } = useContext(UserContext);
-    const { spinner } = useContext(ProgressContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const passwordRef = useRef();
-    const [errorMessage, setErrorMessage] = useState('');
-    const [disabled, setDisabled] = useState(true);
-    const insets = useSafeAreaInsets();
+    const [recording, setRecording] = React.useState();
+    const [recordings, setRecordings] = React.useState([]);
+    const [message, setMessage] = React.useState("");
+    const [data, setData] = React.useState("");
+    const [binary, setBinary] = React.useState("");
+    const [loading, setLoading] = useState(true);  
 
-    useEffect(() => {
-        setDisabled(!(email && password && !errorMessage));
-    }, [email, password, errorMessage]);
+    async function startRecording() {
+        try {
+            const permission = await Audio.requestPermissionsAsync();
 
-    const _handleEmailChange = (email) => {
-        const changedEmail = removeWhitespace(email);
-        setEmail(changedEmail);
-        // setErrorMessage(
-        //     validateEmail(changedEmail) ? '' : 'Please verify your email.'
-        // );
+            if (permission.status === "granted") {
+                await Audio.setAudioModeAsync(
+                    {
+                        allowsRecordingIOS: true,
+                        playsInSilentModeIOS: true
+                    }
+                );
+            
+            const { recording } = await Audio.Recording.createAsync(
+                Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+            );
+
+            setRecording(recording);
+        } else {
+            setMessage("Please grant permission to app to access microphone");
+        }
+        } catch (err) {
+            console.error('Failed to start recording', err);
+        }
+    }
+
+    async function stopRecording() {
+        setRecording(undefined);
+        await recording.stopAndUnloadAsync();
+
+        let updatedRecordings = [...recordings];
+        const { sound, status } = await recording.createNewLoadedSoundAsync();
+        updatedRecordings.push({
+            sound: sound,
+            duration: getDurationFormatted(status.durationMillis),
+            file: recording.getURI()
+        });
+        //console.log(updatedRecordings[updatedRecordings.length - 1].file);
+
+        setRecordings(updatedRecordings);
+
+        const blobToBase64 = (blob) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(blob);
+
+            return new Promise((resolve) => {
+                reader.onloadend = () => {
+                    resolve(reader.result);
+                };
+            });
+        };
+
+        const audioURI = recording.getURI();
+        const blob = await new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.onload = function() {
+                resolve(xhr.response);
+            };
+            xhr.onerror = function() {
+                resolve(xhr.response);
+            };
+            xhr.responseType = "blob";
+            xhr.open("GET", audioURI, true);
+            const reader = new FileReader(audioURI);
+            reader.onload = function() {
+                result = reader.result;
+                console.log(this.result);
+            }
+            xhr.send(null);
+        });
+
+        const audioBase64 = await blobToBase64(blob);
+
+        blob.close()
+
+        setBinary(audioBase64);
+        // console.log(audioBase64);
+    }
+    //   console.log(recordings);
+    //   if (recordings.length > 1) {
+    //     console.log(recordings[recordings.length - 1].file);
+    //   }
+    const formdata = new FormData();
+    formdata.append("voice", binary);
+    // console.log(formdata);
+
+    const getToken = async () => {
+        try {
+            const response = await fetch('http://34.64.69.248:8100/api/voice_chatbot/', {
+                method: 'POST',
+                headers: {
+                    Authorization : 'Token c940dfe459dd8068c392e2e475fb40cd1908155d'
+                },
+                body: formdata,
+            });
+            const json = await response.text();
+            setData(json);
+            setLoading(false);
+            console.log(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
     };
-    const _handlePasswordChange = (password) => {
-        setPassword(removeWhitespace(password));
-    };
 
-    const _handleLoginButtonPress = async () => {
-        login(email, password);
-    };
+    function getDurationFormatted(millis) {
+        const minutes = millis / 1000 / 60;
+        const minutesDisplay = Math.floor(minutes);
+        const seconds = Math.round((minutes - minutesDisplay) * 60);
+        const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+        return `${minutesDisplay}:${secondsDisplay}`;
+    }
 
-    return (
-        <KeyboardAwareScrollView
-            contentContainerStyle={{ flex: 1 }}
-            extraScrollHeight={20}
-        >
-            <View style={styles.container} insets={insets}>
-                <View style={styles.case1}>
-                </View>
-                <View style={styles.case2}>
-                    <ImgGomgomi source={Gomgomi} />
-                </View>
-                <View style={styles.case3}>
-                    <Text style={styles.textCase1} >반갑습니다!</Text>
-                    <Text style={styles.textCase2} >{"\n"}회원 서비스 이용을 위해 로그인 해주세요.</Text>
-                </View>
-                <View style={styles.case4}>
-                    <Input
-                        label="Email"
-                        value={email}
-                        onChangeText={_handleEmailChange}
-                        onSubmitEditing={() => passwordRef.current.focus()}
-                        placeholer="Email"
-                        returnKeyType="next"
-                    />
-                </View>
-                <View style={styles.case5}>
-                    <Input
-                        ref={passwordRef}
-                        label="Password"
-                        value={password}
-                        onChangeText={_handlePasswordChange}
-                        onSubmitEditing={_handleLoginButtonPress}
-                        placeholder="Password"
-                        returnKeyType="done"
-                        isPassword
-                    />
-                </View>
-                <View style={styles.case6}>
-                    <Text>자동으로 로그인하기</Text>
-                </View>
-                <View style={styles.case7}>
-                    <Button
-                        containerStyle={{
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            height: '80%'
-                        }}
-                        title="로그인하기"
-                        titleStyle={{
-                            fontSize: 20,
-                            fontWeight: "bold",
-                            color: '#FFFFFF'
-                        }}
-                        onPress={_handleLoginButtonPress}
-                        disabled={disabled}
-                    />
-                </View>
-                <ErrorText>{errorMessage}</ErrorText>
-                <View style={styles.case8}>
-                    <View
-                        style={{width: "50%", alignItems: 'flex-end'}}
+    function getRecordingLines() {
+        return recordings.map((recordingLine, index) => {
+            return (
+                <View key={index} style={styles.row}>
+                    <Text 
+                        style={styles.fill}
                     >
-                        <Text style={styles.textCase2}>계정이 없으신가요?</Text>
-                    </View>
+                        Recording {index + 1} - {recordingLine.duration}
+                    </Text>
                     <Button
-                        containerStyle={{
-                            width: "50%",
-                            justifyContent: 'center',
-                            paddingLeft: 10
-                        }}
-                        title="회원가입 하기"
-                        titleStyle={{fontSize: 14}}
-                        onPress={() => navigation.navigate('Signup')}
-                        isFilled={false}
+                        style={styles.button}
+                        onPress={() => recordingLine.sound.replayAsync()}
+                        title="Play"
                     />
                 </View>
+            );
+        });
+    }
+        
+    return (
+        <View style={styles.container}>
+            <View 
+                style={{
+                    height: '70%',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Text>{message}</Text>
+                <Button
+                    title={recording ? 'Stop Recording' : 'Start Recording'}
+                    onPress={recording ? stopRecording : startRecording}
+                />
+                <Button
+                    title={'Send Audio Files'}
+                    onPress={getToken}
+                />
             </View>
-        </KeyboardAwareScrollView>
+            <ScrollView style={{height: '30%'}}>
+                {getRecordingLines()}
+            </ScrollView>
+            <StatusBar style="auto" />
+        </View>
     );
-};
-
-export default Login;
-
-const ImgGomgomi = styled.Image`
-`;
-const ErrorText = styled.Text`
-    align-items: flex-start;
-    width: 100%;
-    height: 20px;
-    margin-bottom: 10px;
-    line-height: 20px;
-    color: ${({ theme }) => theme.grayText};
-`;
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        // backgroundColor: theme.background,
-    },
-    textCase1: {
-        fontSize: 25,
-        fontWeight: "bold",
-        letterSpacing: 3,
-    },
-    textCase2: {
-        color: '#86888a'//theme.gray
-    },
-    checkbox: {
-        alignSelf: "center",
-    },
-
-    case1: {
-        width: '100%',
-        height: '19%',
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        justifyContent: 'center',
-        // backgroundColor: theme.testcase1,
-    },
-    case2: {
-        width: '100%',
-        height: '15%',
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        // backgroundColor: theme.testcase2,
     },
-    case3: {
-        width: '100%',
-        height: '15%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        // backgroundColor: theme.testcase3,
-    },
-    case4: {
-        width: '90%',
-        height: '12%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        // backgroundColor: theme.testcase4,
-    },
-    case5: {
-        width: '90%',
-        height: '12%',
-        justifyContent: 'center',
-        // backgroundColor: theme.testcase5,
-    },
-    case6: {
-        width: '100%',
-        height: '3%',
-        paddingLeft: '9%',
-        justifyContent: 'center',
-        backgroundColor: theme.testcase6,
-    },
-    case7: {
-        width: '90%',
-        height: '9%',
-        justifyContent: 'center',
-        // backgroundColor: theme.testcase1,
-    },
-    case8: {
-        width: '100%',
-        height: '5%',
+    row: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        // backgroundColor: theme.testcase3,
+    },
+    fill: {
+        flex: 1,
+        margin: 16
+    },
+    button: {
+        margin: 16
     }
 });
+
+export default Login;
